7ada1b18a2d67ff2efdd309f7c2e1a03bf84756b
ConorMB93 <Conor.dit93@gmail.com>
2020-01-24 16:48:56 +0000

    dynamic routes/links enhancement

--- src/components/App.js
+++ src/components/App.js
@@ -16,8 +16,12 @@ const App = () => {
     return <Section header={'Section 2'} />;
   };
 
-  const Donate = () => {
+  const News = () => {
     return <Section header={'Section 3'} />;
+  };
+
+  const Donate = () => {
+    return <Section header={'Section 4'} />;
   };
 
   const NotFound = () => {
@@ -26,39 +30,62 @@ const App = () => {
 
   //Pass each page component into this array
   //Ensure your landing page is first, and your default page not found, last.
-  const pages = [
-    <Landing />,
-    <Info />,
-    <Donate />,
-    <NotFound />
-  ];
+  // const _pages = [<Landing />, <Info />, <Donate />, <NotFound />];
+
+  const data = {
+    pages: [
+      {
+        componentName: <Landing />,
+        toRender: false
+      },
+      {
+        componentName: <Info />,
+        toRender: true
+      },
+      {
+        componentName: <News />,
+        toRender: true
+      },
+      {
+        componentName: <Donate />,
+        toRender: true
+      },
+      {
+        componentName: <NotFound />,
+        toRender: false
+      }
+    ]
+  };
+
+  const { pages } = data;
+  const firstPageName = pages[0].componentName.type.name;
+  const firsPageComponentName = pages[0].componentName;
 
   return (
     <div className="App">
       <Router>
         <Navbar pages={pages} />
         <Switch>
-
           {/* Route to the Landing page */}
-          <Route key="Landing" exact path={`/`}>
-            <div>
-              {pages[0]}
-            </div>
+          <Route key={firstPageName} exact path={`/`}>
+            <div>{firsPageComponentName}</div>
           </Route>
 
           {/* Dynamically render route link in the Navbar 
           excluding Landing and Not Found pages */}
-          {pages.map((page) => {
-            return <Route key={page.type.name} path={`/${page.type.name}`}>
-              <div>
-                {page}
-              </div>
-            </Route>
+          {pages.map(page => {
+            let { name } = page.componentName.type;
+            let { componentName } = page;
+            return (
+              <Route key={name} path={`/${name}`}>
+                <div>{componentName}</div>
+              </Route>
+            );
           })}
 
           {/* Route to the default page not found, page */}
-          <Route path='*' exact={true}>
-            {pages[pages.length - 1]}
+          <Route path="*" exact={true}>
+            {pages[pages.length - 1].componentName}
           </Route>
         </Switch>
       </Router>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,30 +4,37 @@ import { Link } from 'react-router-dom';
 import logo from '../images/Logo.png';
 import '../styles/Navbar.css';
 
-const Navbar = (props) => {
-
+const Navbar = props => {
   const { pages } = props;
 
   return (
     <div>
       <nav className="Navbar">
-
         {/* Wrap link around logo to return you to the landing page */}
         <Link to="/" className="Navbar-Logo-Link">
           <div className="Navbar-Logo-Image">
-            <img src={logo} alt="Logo" />;
-        </div>
+            <img src={logo} alt="Logo"/>
+          </div>
           <div className="Navbar-Logo">Auzzie Aid</div>
         </Link>
-        <ul className="Navbar-Menu">
 
+        <ul className="Navbar-Menu">
           {/* Dynamically render links based on the pages array */}
-          {pages.map((page) => {
-            if (page.type.name !== pages[0].type.name && page.type.name !== pages[pages.length - 1].type.name) {
+          {pages.map(page => {
+            if (page.toRender) {
+              let { name } = page.componentName.type;
               return (
-                <li key={page.type.name} className="Navbar-Menu-ListItem" onClick={() => { console.log(`You Clicked ${page.type.name}`) }}>
-                  <Link to={`/${page.type.name}`} className="Navbar-Menu-ListItem-Link">{page.type.name}</Link>
-                </li >
+                <li
+                  key={name}
+                  className="Navbar-Menu-ListItem"
+                  onClick={() => {
+                    console.log(`You Clicked ${name}`);
+                  }}
+                >
+                  <Link to={`/${name}`} className="Navbar-Menu-ListItem-Link">
+                    {name}
+                  </Link>
+                </li>
               );
             }
           })}
