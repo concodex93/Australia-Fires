734a8ef447543031722d136bdb7153f1a6ce0d4f
ConorMB93 <Conor.dit93@gmail.com>
2020-02-01 20:25:24 +0000

    pagination for News and sexy load spinner

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,38 +1,47 @@
 import React from 'react';
+import Button from './Button';
+
 import '../styles/NewsList.css';
 
 const NewsList = props => {
-
-    const { articles } = props;
-
-    if (articles.length > 0) {
-        return (
-            <div className="News-Headline-Container">
-                <ul className="News-Headline-List">
-                    {articles.map((article, index) => {
-                        return (
-                            <li key={index} className="News-Headline-List-Items">
-                                <div className="ui card">
-                                    <a href={article.url}>
-                                        <img
-                                            className="Article-Image"
-                                            src={article.urlToImage}
-                                            alt={article.title}
-                                        />
-                                    </a>
-                                    <div className="content">
-                                        <a className="header" href={article.url}>
-                                            {article.title}
-                                        </a>
-                                    </div>
-                                </div>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
-        );
-    }
+  const { articles, next, prev, pagination } = props;
+  if (!articles) return <div>:)</div>;
+  if (articles.length > 0) {
+    return (
+      <div className="News-Headline-Container">
+        <ul className="News-Headline-List">
+          {articles.map((article, index) => {
+            return (
+              <li key={index} className="News-Headline-List-Items">
+                <div className="ui card">
+                  <a href={article.url}>
+                    <img
+                      className="Article-Image"
+                      src={article.urlToImage}
+                      alt={article.title}
+                    />
+                  </a>
+                  <div className="content">
+                    <a className="header" href={article.url}>
+                      {article.title}
+                    </a>
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+        <div className="Button-Page-List">
+          {prev && (
+            <Button title={'Previous'} prev={prev} pagination={pagination} />
+          )}
+          {next && (
+            <Button title={'Next'} next={next} pagination={pagination} />
+          )}
+        </div>
+      </div>
+    );
+  }
 };
 
 export default NewsList;
--- src/components/Section.js
+++ src/components/Section.js
@@ -5,8 +5,14 @@ import '../styles/Section.css';
 const Section = props => {
   const { header, aussieImage, image, newsList } = props;
 
- return (
-    <section className="Section" style={{ backgroundImage: `url(${image})` }}>
+  return (
+    <section
+      className="Section"
+      style={{
+        backgroundImage: `url(${image})`,
+        marginTop: image ? '100px' : ''
+      }}
+    >
       {/*You can also conditionally render by just including the condition inline,
      would we rather do this to avoid having to call a function?*/}
       {header && <h1 className="Section-Title">{header}</h1>}
