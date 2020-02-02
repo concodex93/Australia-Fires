0cc1fdb3f6b4bfd56add576d64bccce0d8acf7b5
Greggles <gregory@wetransfer.com>
2020-02-02 14:37:10 +0100

    Update button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,7 +7,7 @@ const Button = props => {
 
   return (
     <div className="Button-Container">
-      <button className={`ui button`} data-action={text} onClick={onClick}>
+      <button className={`ui button`} onClick={onClick}>
         {text}
       </button>
     </div>
